62bde4c4f86072b33ab59ab5a9dd9f84ccf92c92
Nick Rameau <r@meau.me>
2019-03-12 11:15:46 -0400

    [Patch] Add the schema creating and using commands

--- create_schema.sql
+++ create_schema.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE SCHEMA sema_core;
+
+USE sema_core;
+
 --
 -- Table structure for table `contact`
 --
