be5dccd69abab21c2797cd4e3e1ce5b7f3556618
Nick Rameau <r@meau.me>
2019-03-22 16:52:20 -0400

    Fix db_dump script

--- db_dump.sh
+++ db_dump.sh
@@ -5,6 +5,6 @@ if [ "$(uname)" == "Darwin" ]; then # For Mac
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then # For GNU/Linux
 	export $(grep -v '^#' ../.env | xargs -d '\n')
 fi
-mysqldump -u $DB_USER --password=$DB_PASSWORD -h $DB_HOST --opt $DB_SCHEMA -d --single-transaction | sed 's/ AUTO_INCREMENT=[0-9]*//g' > ../database/create_schema.sql
+mysqldump -u $DB_USER --password=$DB_PASSWORD -h $DB_HOST --opt $DB_SCHEMA -d --single-transaction | sed 's/ AUTO_INCREMENT=[0-9]*//g' > ./create_schema.sql
 # Unset environment variables from the report_server .env
 unset $(grep -v '^#' ../.env | sed -E 's/(.*)=.*/\1/' | xargs)
